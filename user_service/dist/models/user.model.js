c067730c11baa3b2fe98692c4b74f9aa1059f57b
Varunyadavgithub <yadav.varun056038@gmail.com>
2026-01-03 21:43:15 +0530

    feat: add user model with TypeScript interface and Mongoose schema

---
+++ user_service/dist/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["user", "admin"],
+    },
+    playlist: [
+        {
+            type: String,
+            required: true,
+        },
+    ],
+}, { timestamps: true });
+export const User = mongoose.model("User", userSchema);
---
+++ user_service/src/models/user.model.ts
@@ -0,0 +1,41 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  role: string;
+  playlist: string[];
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["user", "admin"],
+    },
+    playlist: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model<IUser>("User", userSchema);

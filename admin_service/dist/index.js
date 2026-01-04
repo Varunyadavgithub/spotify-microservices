79642977323fd59bb9c344a38af50fba6dc77dac
Varunyadavgithub <yadav.varun056038@gmail.com>
2026-01-04 16:25:34 +0530

    feat: integrate @neondatabase/serverless for database management and initialize albums and songs tables

--- admin_service/dist/index.js
+++ admin_service/dist/index.js
@@ -1,18 +1,14 @@
 import express from "express";
-import { Request, Response } from "express";
 import dotenv from "dotenv";
 import { sql } from "./config/db.js";
 dotenv.config();
-
 // Initialize Express app
 const app = express();
-
 // Middlewares
 app.use(express.json());
-
 async function initializeDatabase() {
-  try {
-    await sql`
+    try {
+        await sql `
             CREATE TABLE IF NOT EXISTS albums (
                 id SERIAL PRIMARY KEY,
                 title VARCHAR(255) NOT NULL,
@@ -21,8 +17,7 @@ async function initializeDatabase() {
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
         `;
-
-    await sql`
+        await sql `
             CREATE TABLE IF NOT EXISTS songs (
                 id SERIAL PRIMARY KEY,
                 title VARCHAR(255) NOT NULL,
@@ -33,23 +28,20 @@ async function initializeDatabase() {
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
         `;
-
-    console.log("Database initialized successfully.");
-  } catch (error) {
-    console.error("Error initializing database:", error);
-  }
+        console.log("Database initialized successfully.");
+    }
+    catch (error) {
+        console.error("Error initializing database:", error);
+    }
 }
-
 // Default route
-app.get("/", (req: Request, res: Response) => {
-  res.send("Admin Service is up and running!");
+app.get("/", (req, res) => {
+    res.send("Admin Service is up and running!");
 });
-
 // Start the server
 const PORT = process.env.PORT || 6000;
-
 initializeDatabase().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Admin service is running on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`Admin service is running on port ${PORT}`);
+    });
 });

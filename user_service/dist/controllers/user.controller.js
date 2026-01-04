fd7ea329e2e471762e9dc82ba8ebccc7bdfde179
Varunyadavgithub <yadav.varun056038@gmail.com>
2026-01-04 15:17:32 +0530

    feat: implement login functionality with JWT authentication

--- user_service/dist/controllers/user.controller.js
+++ user_service/dist/controllers/user.controller.js
@@ -23,3 +23,20 @@ export const registerUser = TryCatch(async (req, res) => {
         .status(201)
         .json({ message: "User Registered Successfully...!!!", user, token });
 });
+export const loginUser = TryCatch(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(404).json({ message: "User not exists...!!!" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(400).json({ message: "Invalid Credentials...!!!" });
+    }
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
+    return res
+        .status(200)
+        .json({ message: "Login Successful...!!!", user, token });
+});

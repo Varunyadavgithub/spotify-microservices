a7184bc3c075a92baea323466afa792cf53a4412
Varunyadavgithub <yadav.varun056038@gmail.com>
2026-01-20 20:05:13 +0530

    implement the controller to create a song and upload their thumbnail

--- admin_service/src/routes/admin.route.ts
+++ admin_service/src/routes/admin.route.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
-import { addAlbum } from "../controllers/admin.controller.js";
+import {
+  addAlbum,
+  addSong,
+  addSongThumbnail,
+} from "../controllers/admin.controller.js";
 import uploadFile from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
 router.post("/album/new", isAuthenticated, uploadFile, addAlbum);
+router.post("/song/new", isAuthenticated, uploadFile, addSong);
+router.post("/song/:id", isAuthenticated, uploadFile, addSongThumbnail);
 
 export default router;
